1b025c740c8089b9c4401d3f980487bcbc64e728
pkapadia9871 <30789273+pkapadia9871@users.noreply.github.com>
2019-01-19 22:18:38 -0600

    Delete Ex2.py

--- Chapter 8/Ex2.py
+++
@@ -1,6 +0,0 @@
-##Design a program that generates a seven-digit lottery number. 
-##The program should generate seven random numbers, each in the range of 0
-##through 9, and assign each number to a list element.
-##(Random numbers were discussed in Chapter 6.)
-##Then write another loop that displays the contents of the list. 
-
